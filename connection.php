e5136154b051b9bccbe1175792e2b2f038f32715
Jims <jamesgenabio31@gmail.com>
2024-12-08 20:32:34 +0800

    feat : student_information done

--- connection.php
+++ connection.php
@@ -7,3 +7,5 @@ $connection = mysqli_connect("localhost", "root", "", "student_information");
 if(!$connection){
     echo "<script>alert('Not connected to database')</script>";
 }
+
+error_reporting(0);
